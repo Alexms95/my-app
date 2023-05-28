1ff41e27961ffe3c4c6c7740dd25c698af73d13b
Alex Silva <alex.silva@radixeng.com.br>
2023-05-28 20:54:48 -0300

    Adding tailwindcss, deleting something useless and initiating the booklist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const BookList = () => (
+  <section className="w-[90vw] max-w-[1170px] mx-[5rem] my-auto grid gap-[2rem] md:grid-cols-3">
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article className="rounded-[1rem] bg-[#fff] px-[1rem] py-[2rem]">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+        alt=""
+      />
+      <h1 className="font-bold">I Love to the Moon and Back</h1>
+      <h4>Amelia Hepworth</h4>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
